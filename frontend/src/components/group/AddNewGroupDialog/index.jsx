5a6ff0b51769711d62c15351900485ba0659cc3f
feng xia <feng_xia41103@hotmail.com>
2022-01-29 11:22:22 -0500

    feat(FRONTEND): Add new group dialog

---
+++ frontend/src/components/group/AddNewGroupDialog/index.jsx
@@ -0,0 +1,104 @@
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import Chip from "@material-ui/core/Chip";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import TextField from "@material-ui/core/TextField";
+import AddIcon from "@material-ui/icons/Add";
+import { map, filter } from "lodash";
+import React, { useState, useContext } from "react";
+import { useMutate } from "restful-react";
+
+import ShowResource from "src/components/common/ShowResource";
+import GlobalContext from "src/context";
+
+export default function AddNewGroupDialog() {
+  const { api } = useContext(GlobalContext);
+  const [open, setOpen] = useState(false);
+  const [resource] = useState("/groups");
+  const [group, setGroup] = useState("");
+
+  const { mutate: create } = useMutate({
+    verb: "POST",
+    path: `${api}${resource}/`,
+  });
+
+  // const reload = () => window.location.reload();
+  const handleClickOpen = () => setOpen(true);
+
+  const handleClose = () => setOpen(false);
+
+  const on_group_change = (event) => {
+    // symbol is always in upper case
+    let tmp = event.target.value;
+    setGroup(tmp.toUpperCase().trim());
+  };
+
+  // call API and close this dialog
+  const on_create = () => {
+    create({ name: group });
+    setOpen(false);
+    // reload();
+  };
+
+  const render_data = (data) => {
+    const groups = map(
+      filter(data.results, (s) => s.name.includes(group)),
+      (s) => <Chip key={s.id} group="primary" label={s.name} />
+    );
+
+    const is_error = map(data.results, (s) => s.name.toUpperCase()).includes(
+      group
+    );
+
+    return (
+      <>
+        <Button group="secondary" onClick={handleClickOpen}>
+          <AddIcon />
+          Add New Group
+        </Button>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">Add New Group</DialogTitle>
+          <DialogContent>
+            <DialogContentText>Group name must be unique.</DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              value={group}
+              onChange={on_group_change}
+              placeholder="group name"
+              fullWidth
+              error={is_error}
+              label={is_error ? "Error" : ""}
+              helperText={is_error ? "Group name must be unique." : ""}
+            />
+
+            <Box mt={2}>{groups}</Box>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} group="primary">
+              Cancel
+            </Button>
+            <Button
+              variant="contained"
+              group="primary"
+              onClick={on_create}
+              disabled={is_error}
+            >
+              Add
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </>
+    );
+  };
+  // render as usual to get data
+  return <ShowResource {...{ resource, on_success: render_data }} />;
+}
