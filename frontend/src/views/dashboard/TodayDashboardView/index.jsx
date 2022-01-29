de85e6c3ccab70c9fe93a585dbf3f21fd2882246
feng xia <feng_xia41103@hotmail.com>
2022-01-29 11:24:50 -0500

    feat(FRONTEND): Add commands to a dropdown menu

--- frontend/src/views/dashboard/TodayDashboardView/index.jsx
+++ frontend/src/views/dashboard/TodayDashboardView/index.jsx
@@ -1,17 +1,35 @@
-import { Box, Button } from "@material-ui/core";
+import { Box, Button, List, ListItem } from "@material-ui/core";
 import { Container } from "@material-ui/core";
 import Stack from "@mui/material/Stack";
 import React, { useState } from "react";
 
 import AddNewColorDialog from "src/components/color/AddNewColorDialog";
-import PersonList from "src/components/color/PersonList";
+import PersonListByColor from "src/components/color/PersonListByColor";
+import DropdownMenu from "src/components/common/DropdownMenu";
 import Page from "src/components/common/Page";
 import ShowResource from "src/components/common/ShowResource";
+import AddNewGroupDialog from "src/components/group/AddNewGroupDialog";
+import AddNewPersonDialog from "src/components/person/AddNewPersonDialog";
+import PersonList from "src/components/person/PersonList";
 
 export default function TodayDashboardView() {
   // states
   const [resource] = useState("/colors");
   const [color, setColor] = useState("");
+
+  const menuContent = (
+    <List>
+      <ListItem>
+        <AddNewGroupDialog />
+      </ListItem>
+      <ListItem>
+        <AddNewColorDialog />
+      </ListItem>
+      <ListItem>
+        <AddNewPersonDialog />
+      </ListItem>
+    </List>
+  );
 
   // renders
   const render_data = (data) => {
@@ -30,11 +48,11 @@ export default function TodayDashboardView() {
         <Container maxWidth={false}>
           <Stack direction="row" spacing={3}>
             {colors}
-            <AddNewColorDialog />
+            <DropdownMenu content={menuContent} />
           </Stack>
 
           <Box mt={3}>Set color: {color.name}</Box>
-          {color ? <PersonList color={color.id} /> : null}
+          {color ? <PersonListByColor color={color.id} /> : <PersonList />}
         </Container>
       </Page>
     );
