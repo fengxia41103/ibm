b6fed93c23cba61bb0d0122de18b763e6dd3afc1
feng xia <feng_xia41103@hotmail.com>
2022-01-29 11:22:45 -0500

    feat(FRONTEND): Add person components
    - Create new person dialog
    - Button to display person's name
    - All person list when no color is selected

---
+++ frontend/src/components/person/AddNewPersonDialog/index.jsx
@@ -0,0 +1,104 @@
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import Chip from "@material-ui/core/Chip";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import TextField from "@material-ui/core/TextField";
+import AddIcon from "@material-ui/icons/Add";
+import { map, filter } from "lodash";
+import React, { useState, useContext } from "react";
+import { useMutate } from "restful-react";
+
+import ShowResource from "src/components/common/ShowResource";
+import GlobalContext from "src/context";
+
+export default function AddNewPersonDialog() {
+  const { api } = useContext(GlobalContext);
+  const [open, setOpen] = useState(false);
+  const [resource] = useState("/persons");
+  const [person, setPerson] = useState("");
+
+  const { mutate: create } = useMutate({
+    verb: "POST",
+    path: `${api}${resource}/`,
+  });
+
+  // const reload = () => window.location.reload();
+  const handleClickOpen = () => setOpen(true);
+
+  const handleClose = () => setOpen(false);
+
+  const on_person_change = (event) => {
+    // symbol is always in upper case
+    let tmp = event.target.value;
+    setPerson(tmp.toUpperCase().trim());
+  };
+
+  // call API and close this dialog
+  const on_create = () => {
+    create({ name: person });
+    setOpen(false);
+    // reload();
+  };
+
+  const render_data = (data) => {
+    const persons = map(
+      filter(data.results, (s) => s.name.includes(person)),
+      (s) => <Chip key={s.id} person="primary" label={s.name} />
+    );
+
+    const is_error = map(data.results, (s) => s.name.toUpperCase()).includes(
+      person
+    );
+
+    return (
+      <>
+        <Button person="secondary" onClick={handleClickOpen}>
+          <AddIcon />
+          Add New Person
+        </Button>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">Add New Person</DialogTitle>
+          <DialogContent>
+            <DialogContentText>Person name must be unique.</DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              value={person}
+              onChange={on_person_change}
+              placeholder="person name"
+              fullWidth
+              error={is_error}
+              label={is_error ? "Error" : ""}
+              helperText={is_error ? "Person name must be unique." : ""}
+            />
+
+            <Box mt={2}>{persons}</Box>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} person="primary">
+              Cancel
+            </Button>
+            <Button
+              variant="contained"
+              person="primary"
+              onClick={on_create}
+              disabled={is_error}
+            >
+              Add
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </>
+    );
+  };
+  // render as usual to get data
+  return <ShowResource {...{ resource, on_success: render_data }} />;
+}
---
+++ frontend/src/components/person/PersonList/index.jsx
@@ -0,0 +1,53 @@
+import {
+  Grid,
+  Card,
+  CardHeader,
+  CardContent,
+  Typography,
+} from "@material-ui/core";
+import Stack from "@mui/material/Stack";
+import { map, groupBy, isNull } from "lodash";
+import React, { useState } from "react";
+
+import ShowResource from "src/components/common/ShowResource";
+import NameButton from "src/components/person/NameButton";
+
+export default function PersonList() {
+  // states
+  const [resource] = useState("/persons");
+
+  // renders
+  const render_data = (data) => {
+    const persons = data.results;
+    const groupByGroup = groupBy(persons, (p) =>
+      isNull(p.group) ? "None" : p.group.name
+    );
+
+    const groupCards = map(groupByGroup, (persons, group) => {
+      const names = map(persons, (p) => <NameButton key={p.name} who={p} />);
+
+      return (
+        <Grid item key={group} lg={4} md={6} xs={12}>
+          <Card>
+            <CardHeader
+              title={<Typography variant="h3">GROUP: {group}</Typography>}
+            />
+            <CardContent>
+              <Stack direction="row" spacing={1}>
+                {names}
+              </Stack>
+            </CardContent>
+          </Card>
+        </Grid>
+      );
+    });
+
+    return (
+      <Grid container spacing={3}>
+        {groupCards}
+      </Grid>
+    );
+  };
+
+  return <ShowResource {...{ resource, on_success: render_data }} />;
+}
